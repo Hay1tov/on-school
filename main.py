dc1d61b33c08db9bd231ca4a989d60a80a7ebeb1
Hay1tov <eldorh35@gmail.com>
2025-01-09 23:31:27 +0500

    Bajarildi

--- main.py
+++ main.py
@@ -1,72 +1,61 @@
-from os import system
 from school import students, courses, grades
+def main():
+   
+    student = {}
 
-def show_menu(
-        menu: list[tuple[int, str]],
-        user: dict = {}
-    ) -> None:
-
-    print(
-        "=== Welcome to On-School ===" if len(menu) == 3 else f"\n--- Main Menu for {user['name']} ---",
-        end="\n\n"
-    )
+    students_data = []
     
-    for option in menu:
-        print(f'{option[0]}. {option[1]}', end="\n")
-
-def main() -> None:
-    """
-    Main function that drives the On-School system. It displays the main menu, 
-    handles user registration and login, and provides an interface for enrolled students 
-    to interact with courses and check grades.
-    """
-    students_data = {
-        100001: {
-            'email': 'example@gmail.com',
-            'name': 'Ali Valiyev',
-            'password': '1234'
-        },
-        100002: {
-            'email': 'bjfkabdsfabds',
-            'name': 'jkfbadjsbf',
-            'password': '324512345'
-        }
-    }
     courses_data = [
         {"course_name": "Python Basics", "instructor": "John Doe", "duration": "8 weeks", "price": 500},
         {"course_name": "Data Science 101", "instructor": "Jane Smith", "duration": "10 weeks", "price": 780}
     ]
-    grades_data = {}
 
-    menu = [
-        (1, "Register"), (2, "Login"), (3, "Exit")
-    ]
-
-    user_menu = [
-        (1, "View Available Courses"),
-        (2, "Enroll in a Course"),
-        (3, "View My Courses"),
-        (4, "Check My Grades"),
-        (5, "Logout")
-    ]
-
-    system('clear')
+    grades_data = []
 
     while True:
-        show_menu(menu)
-        choice = input("\nSelect an option: ")
+        print("\n=== Welcome to On-School ===","1. Register","2. Login","3. Exit", sep="\n")
+        choise = int(input("Select an option: "))
         
-        if choice == "1":
-            students_data = students.register_student(students_data)
-        elif choice == "2":
-            user_id = students.login_student(students_data)
-            if user_id != -1:
-                while True:
-                    show_menu(user_menu, user=students_data[user_id])
-                    user_choice = input("Choose an option: ")
-                    if user_choice == '1':
-                        # TODO: kurslar royatini chiqarish
-                        pass
+        if choise == 1:
+            new_student = students.register_student(student, students_data)
+            students_data.append(new_student)
+
+            print(f"Registration successful! Welcome, {(new_student["name"])}", end="\n")
+        elif choise == 2:
+            
+            user = students.login_student(students_data)
+            while user:
+                
+                print("\n--- Main Menu for Alice Smith --- \n")
+                print("1. View Available Courses")
+                print("2. Enroll in a Course")
+                print("3. View My Courses")
+                print("4. Check My Grades")
+                print("5. Logout")
+                print("6. exit")
+                choose_commond_user = int(input("Choose an option: "))
+
+                if choose_commond_user == 1:
+                    courses.view_courses(courses_data)
+                elif choose_commond_user == 2:
+                    user['course'].append(students.enroll_in_course(courses_data, students_data, user['password_login']['login']))
+                elif choose_commond_user == 3:
+                    courses.view_courses(user['course'])  
+                elif choose_commond_user == 4:
+                    grades.check_grades(user, grades_data)
+                elif choose_commond_user == 5:
+                    break  
+                elif choose_commond_user == 6:
+                    exit()
+                else:
+                    print("\nbunday buyruq yuq\n")
+                  
+        elif choise == 3:
+        
+            print("exiting...")
+            exit()
+main()
 
 if __name__ == "__main__":
     main()
+    pass
